4f804938ff482ad48793e10e68fcf31e6c9ec944
hamedasemi <h-@live.se>
2020-06-21 17:09:54 +0200

    Add one done item

--- app/components/todo/todo.jsx
+++ app/components/todo/todo.jsx
@@ -9,7 +9,7 @@ const TodoProvider = ({ children }) => {
     let initialState = {
         items: [{ content: "Bring an umbrella", id: 1592751723302, done: false },
         { content: "Organise photo shoot", id: 1592751709592, done: false },
-        { content: "Design a prototype", id: 1592751651633, done: false },
+        { content: "Design a prototype", id: 1592751651633, done: true },
         { content: "2:00 Meet with Laura", id: 1592751597364, done: false },
         { content: "Polish brand idea", id: 1592751605817, done: false }], filteredItems: []
     };
